459f5d7f5b3aa28b93bd5acf86fd577fe723eea6
AyushAryal <ayusharyal100@gmail.com>
2024-01-05 18:33:32 +0545

    formatted

--- volunteer_manangement_system/administrator/templatetags/geticon.py
+++ volunteer_manangement_system/administrator/templatetags/geticon.py
@@ -1,6 +1,12 @@
 from django import template
 
 from django.contrib.auth import get_user_model
+from user.models import (
+    Profile,
+    Incident,
+    Job,
+    Programme,
+)
 
 register = template.Library()
 
@@ -9,12 +15,14 @@ register = template.Library()
 def modelicon(model):
     return {
         get_user_model(): "fa-solid fa-user",
-        # Cart: "fa-solid fa-cart-shopping",
+        Profile: "fa-solid fa-id-card-clip",
+        Incident: "fa-solid fa-map-pin",
+        Job: "fa-solid fa-briefcase",
     }.get(model.get("model", None), "fa-solid fa-box")
 
 
 @register.filter()
 def appicon(app):
     return {
-        # "user": "fa-solid fa-user",
+        "user": "fa-solid fa-user",
     }.get(app["app_label"], "fa-solid fa-box")
