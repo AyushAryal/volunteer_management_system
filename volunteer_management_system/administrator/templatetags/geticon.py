6ddafaf8b5eea1188a6d7c46ce4608c9636b808f
AyushAryal <ayusharyal100@gmail.com>
2024-01-13 13:38:57 +0545

    fix icons

--- volunteer_management_system/administrator/templatetags/geticon.py
+++ volunteer_management_system/administrator/templatetags/geticon.py
@@ -2,6 +2,7 @@ import federal.models
 import incident.models
 from django import template
 from django.contrib.auth import get_user_model
+from django.contrib.sites.models import Site
 
 register = template.Library()
 
@@ -11,16 +12,22 @@ def modelicon(model):
     return {
         get_user_model(): "fa-solid fa-user",
         incident.models.VolunteerProfile: "fa-solid fa-id-card-clip",
-        incident.models.Incident: "fa-solid fa-map-pin",
+        incident.models.Incident: "fa-solid fa-user-injured",
         incident.models.Job: "fa-solid fa-briefcase",
-        federal.models.Province: "fa-solid fa-map",
+        incident.models.Program: "fa-solid fa-tent",
+        federal.models.Province: "fa-solid fa-landmark-dome",
+        federal.models.District: "fa-solid fa-landmark-dome",
+        federal.models.Municipality: "fa-solid fa-landmark-dome",
+        federal.models.Ward: "fa-solid fa-landmark-dome",
+        Site: "fa-solid fa-globe",
     }.get(model.get("model", None), "fa-solid fa-box")
 
 
 @register.filter()
 def appicon(app):
     return {
-        "federal": "fa-solid fa-map",
+        "federal": "fa-solid fa-map-location-dot",
         "incident": "fa-solid fa-clock",
         "authentication": "fa-solid fa-lock",
+        "sites": "fa-solid fa-globe",
     }.get(app["app_label"], "fa-solid fa-box")
