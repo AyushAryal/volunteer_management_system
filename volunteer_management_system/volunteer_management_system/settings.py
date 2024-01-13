40a99fe33b23ce944355c9a4f42478e24269fb30
AyushAryal <ayusharyal100@gmail.com>
2024-01-13 13:24:44 +0545

    add pagination

--- volunteer_management_system/volunteer_management_system/settings.py
+++ volunteer_management_system/volunteer_management_system/settings.py
@@ -167,6 +167,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 LEAFLET_CONFIG = {
