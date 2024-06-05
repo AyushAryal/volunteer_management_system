0e56a4e4f804d61b739849b30f13abf16f968328
Ayush Aryal <ayusharyal100@gmail.com>
2024-06-05 08:35:29 +0545

    commit build script to repository

---
+++ build.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+PARENT=$(dirname "$0")
+export $(grep -v '^#' $PARENT/volunteer_management_system/.env | xargs -d '\n')
+
+if [[ $DJANGO_SETTINGS_MODULE == "volunteer_management_system.development" ]]; then
+    rm $PARENT/volunteer_management_system/federal/migrations/0001_initial.py
+    rm $PARENT/volunteer_management_system/incident/migrations/0001_initial.py
+    rm $PARENT/volunteer_management_system/authentication/migrations/0001_initial.py
+    sudo su postgres -c "psql -c 'DROP DATABASE vms;' && createdb vms" &&
+    python $PARENT/volunteer_management_system/manage.py makemigrations federal &&
+    python $PARENT/volunteer_management_system/manage.py makemigrations incident &&
+    python $PARENT/volunteer_management_system/manage.py makemigrations authentication &&
+    python $PARENT/volunteer_management_system/manage.py migrate &&
+    python $PARENT/volunteer_management_system/manage.py populatedb
+else
+    echo "--------------------------------------------------" 1>&2;
+    echo "YOU ARE IN PRODUCTION!!" 1>&2;
+    echo "--------------------------------------------------" 1>&2;
+fi
+
