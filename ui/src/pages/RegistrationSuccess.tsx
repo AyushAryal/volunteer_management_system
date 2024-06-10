38e9460c0626e67cd72e7567d799483114eddb74
Ayush Aryal <ayusharyal100@gmail.com>
2024-06-10 12:45:01 +0545

    resgistrartion success page

--- ui/src/pages/RegistrationSuccess.tsx
+++ ui/src/pages/RegistrationSuccess.tsx
@@ -1,5 +1,25 @@
+import { volunteering } from "@assets/index";
+import { Footer, Navbar } from "@components/landing";
 
 
 export function RegistrationSuccess() {
-    return <div><h1>Registration was successfull. go back to main page</h1></div>
+    return <div>
+        <Navbar />
+        <div
+            style={{
+                backgroundImage: `url(${volunteering})`,
+            }}
+            className="flex flex-column h-screen justify-content-center align-items-center bg-cover">
+            <div className="flex flex-column w-5 p-5 border-round-3xl text-100 align-items-center"
+                style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+                <h1 className="text-teal-500">Registration was successful!</h1>
+                <div> Welcome to NDRRMA Volunteer Management System.</div>
+                <a href="https://vms.ndrrma.gov.np"
+                    className="font-semibold text-orange-300 no-underline">
+                    Go back to main page
+                </a>
+            </div>
+        </div>
+        <Footer />
+    </div>
 }
