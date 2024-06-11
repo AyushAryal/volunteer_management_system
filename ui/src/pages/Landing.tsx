65f3d294e4c7026361a655e8e068e33dd1db91e2
nitefury7 <ayush.infinity5@gmail.com>
2024-06-11 10:11:09 +0545

    fix toast type

--- ui/src/pages/Landing.tsx
+++ ui/src/pages/Landing.tsx
@@ -3,10 +3,10 @@ import { Navbar, Hero, Overview, Stats, Footer } from "../components/landing";
 import { Toast } from 'primereact/toast';
 
 const Landing = () => {
-  const toast = useRef(null);
+  const toast = useRef<Toast>(null);
 
   useEffect(() => {
-    toast.current.show({ severity: 'info', summary: 'Welcome to NDRRMA VMS.', detail: 'Old VMS users can access their accounts by resetting their password and should update their profiles.', life: 10000 });
+    toast.current?.show({ severity: 'info', summary: 'Welcome to NDRRMA VMS.', detail: 'Old VMS users can access their accounts by resetting their password and should update their profiles.', life: 10000 });
   }, []);
 
   return (
